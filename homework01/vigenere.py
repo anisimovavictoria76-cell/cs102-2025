a9abd72b6f0b7a9e1a086bf186256c5800c33b89
anisimovavictoria76-cell <anisimovavictoria76@gmail.com>
2025-10-28 12:57:40 +0300

    Реализованная функция encrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,25 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword_upper = keyword.upper()
+    key_length = len(keyword_upper)
+
+    for i, char in enumerate(plaintext):
+        if char.isalpha():
+            key_char = keyword_upper[i % key_length]
+            shift = ord(key_char) - ord('A')
+
+            if char.isupper():
+                base = ord('A')
+                encrypted_char = chr((ord(char) - base + shift) % 26 + base)
+                ciphertext += encrypted_char
+            else:
+                base = ord('a')
+                encrypted_char = chr((ord(char) - base + shift) % 26 + base)
+                ciphertext += encrypted_char
+        else:
+            ciphertext += char
+
     return ciphertext
 
 
@@ -24,5 +42,22 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword_upper = keyword.upper()
+    key_length = len(keyword_upper)
+
+    for i, char in enumerate(ciphertext):
+        if char.isalpha():
+            key_char = keyword_upper[i % key_length]
+            shift = ord(key_char) - ord('A')
+
+            if char.isupper():
+                base = ord('A')
+                decrypted_char = chr((ord(char) - base - shift) % 26 + base)
+                plaintext += decrypted_char
+            else:
+                base = ord('a')
+                decrypted_char = chr((ord(char) - base - shift) % 26 + base)
+                plaintext += decrypted_char
+        else:
+            plaintext += char
     return plaintext

ae012a960f747f7bfad4cef7df3d3eac0d82d615
anisimovavictoria76-cell <anisimovavictoria76@gmail.com>
2025-11-18 10:32:59 +0300

    add file

--- homework01/affline_cipher.py
+++ homework01/affline_cipher.py
@@ -1,35 +1,19 @@
 def encrypt_affine(plaintext, a, b):
-    m = 26
-
-    def char_to_num(char):
-        if char.isupper():
-            return ord(char) - ord('A')
-        elif char.islower():
-            return ord(char) - ord('a')
-        return None
-
-    def num_to_char(num, is_upper):
-        if is_upper:
-            return chr(num + ord('A'))
-        else:
-            return chr(num + ord('a'))
-
-    encrypted_chars = []
+    result = []
 
     for char in plaintext:
-        if char.isalpha():
-            is_upper = char.isupper()
-
-            x = char_to_num(char)
-
-            encrypted_num = (a * x + b) % m
-
-            encrypted_char = num_to_char(encrypted_num, is_upper)
-            encrypted_chars.append(encrypted_char)
+        if char.isupper():
+            x = ord(char) - ord('A')
+            encrypted = (a * x + b) % 26
+            result.append(chr(encrypted + ord('A')))
+        elif char.islower():
+            x = ord(char) - ord('a')
+            encrypted = (a * x + b) % 26
+            result.append(chr(encrypted + ord('a')))
         else:
-            encrypted_chars.append(char)
+            result.append(char)
 
-    return ''.join(encrypted_chars)
+    return ''.join(result)
 
 
 if __name__ == "__main__":
@@ -39,8 +23,3 @@ if __name__ == "__main__":
     encrypted = encrypt_affine(plaintext, a, b)
     print(f"Plaintext: {plaintext}")
     print(f"Encrypted: {encrypted}")
-
-    plaintext2 = "Hello World"
-    encrypted2 = encrypt_affine(plaintext2, a, b)
-    print(f"Plaintext: {plaintext2}")
-    print(f"Encrypted: {encrypted2}")
